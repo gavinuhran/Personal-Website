3ca88efdcfd4f284948f4941315d359523d801c8
Gavin Uhran <gavinu2001@gmail.com>
2020-05-07 15:27:43 -0500

    Version 1.3 | Added project/ subpage

--- personal_website/base/views.py
+++ personal_website/base/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base/home.html')
+
+def projects(request):
+    return render(request, 'base/projects.html')
