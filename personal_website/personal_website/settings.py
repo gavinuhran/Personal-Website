efcdf5f90307d801e29c3595fd3c939c07793432
Gavin Uhran <gavinu2001@gmail.com>
2020-07-30 15:53:08 -0500

    Turn off DEBUG

--- personal_website/personal_website/settings.py
+++ personal_website/personal_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'online-portfolio-gavinuhran.herokuapp.com', 'https://online-portfolio-gavinuhran.herokuapp.com/']
 
