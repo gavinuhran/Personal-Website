f5bf3ee65d7f03c1213ec06aaee0084ccb4ee9ca
Gavin Uhran <gavinu2001@gmail.com>
2020-07-30 16:55:27 -0500

    Updated urls.py

--- test.py
+++
@@ -1,5 +0,0 @@
-STATIC_HOST = 'https://d1wh6hiyc9dtto.cloudfront.net'
-STATIC_URL = STATIC_HOST + '/staticFiles/'
-print(STATIC_URL)
-MEDIA_URL = STATIC_HOST + '/media/headshot.JPG'
-print(MEDIA_URL)
