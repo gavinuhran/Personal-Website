16ee8304ee3c8ea963ffadfd52e5a680e32f109a
Gavin Uhran <gavinu2001@gmail.com>
2020-07-30 16:44:51 -0500

    Adjusted urls.py

---
+++ test.py
@@ -0,0 +1,5 @@
+STATIC_HOST = 'https://d1wh6hiyc9dtto.cloudfront.net'
+STATIC_URL = STATIC_HOST + '/staticFiles/'
+print(STATIC_URL)
+MEDIA_URL = STATIC_HOST + '/media/headshot.JPG'
+print(MEDIA_URL)
